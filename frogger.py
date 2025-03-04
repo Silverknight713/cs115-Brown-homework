78d0e7b1e3fd14ce10ce59dde9c197c6d70d9e4c
Silverknight713 <silverknight713@gmail.com>
2025-03-04 07:02:09 +0000

    Same as last commit, I just forgot this file :P

--- frogger.py
+++ frogger.py
@@ -22,6 +22,23 @@ while running:
   """ draw to our screen """
   # clear screen
   screen.fill("white")
+
+  # Grass zones
+  pygame.draw.rect(screen, "green", pygame.Rect((0, 0), (600, 50)))
+  pygame.draw.rect(screen, "green", pygame.Rect((0, 350), (600, 50)))
+  #Road
+  pygame.draw.rect(screen, "grey", pygame.Rect((0, 50), (600, 300)))
+  pygame.draw.rect(screen, "yellow", pygame.Rect((0, 190), (600, 20)))
+
+  #cars
+  pygame.draw.rect(screen, "black", pygame.Rect((0, 65), (100, 50)),10)
+  pygame.draw.rect(screen, "purple", pygame.Rect((0, 125), (200, 50)),10)
+  pygame.draw.rect(screen, "red", pygame.Rect((0, 225), (100, 50)),10)
+  pygame.draw.rect(screen, "blue", pygame.Rect((0, 285), (100, 50)),10)
+
+  #frog
+  pygame.draw.circle(screen, "#0ba300", (300,375), 25)
+  
   # update screen
   pygame.display.flip()
   # fps
