97c6e1913782e7903a85f9660d255304bae65fa5
Silverknight713 <silverknight713@gmail.com>
2025-03-11 06:47:18 +0000

    Reforming background and car position, then adding movement. Bug with first 2 cars?

--- Brown_Homework#7_PyGame.py
+++ Brown_Homework#7_PyGame.py
@@ -5,8 +5,11 @@ Assignment #7
 Description: beginning the code on frogger game and initializing game loop. Making basic asset shapes for cars and frog, and designing the background for the game.
 """
 import pygame
+from pygame.constants import KEYDOWN
+
 #initialize pygame
 pygame.init()
+
 #window dimensions and display
 width = 600
 height = 400
@@ -17,6 +20,16 @@ pygame.display.set_caption("frogger")
 clock = pygame.time.Clock()
 dt = 0
 speed = 10
+
+#Frog Position
+cur_pos = [300,380]
+
+#Car Positions
+c1_pos = [0,45]
+c2_pos = [600,125]
+c3_pos = [0,225]
+c4_pos = [600,305]
+
 """game loop"""
 running = True
 while running:
@@ -24,26 +37,73 @@ while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+    if event.type == KEYDOWN:
+      if event.key == pygame.K_ESCAPE: #escape key
+        running = False
+      if event.key == pygame.K_w: #moving up
+        cur_pos[1] -= 20
+      if event.key == pygame.K_s: #moving down
+        cur_pos[1] += 20
+      if event.key == pygame.K_a: # moving left 
+        cur_pos[0] -= 20
+      if event.key == pygame.K_d: # moving right
+        cur_pos[0] += 20
+  """Car Movements"""
+  #car1
+  if c1_pos[0] > 600:
+    c1_pos[0] = -100
+  else:
+    c1_pos[0] += 10
+  #car2
+  if c2_pos[0] < -200:
+    c2_pos[0] = 600
+  else:
+    c1_pos[0] -= 10
+  #car3
+  if c3_pos[0] > 600:
+    c3_pos[0] = -100
+  else:
+    c3_pos[0] += 10
+  #car4
+  if c4_pos[0] < -100:
+    c4_pos[0] = 600
+  else:
+    c4_pos[0] -= 10
   """update our game state"""
+    
+  #update bounds
+  if cur_pos[0] < 20: #x direction bounds
+    cur_pos[0] = 20
+  if cur_pos[0] > width - 20:
+    cur_pos[0] = width - 20
+  if cur_pos[1] < 20: #y direction bounds
+    cur_pos[1] = 20
+  if cur_pos[1] > height - 20:
+    cur_pos[1] = height - 20
+  
   """ draw to our screen """
   # clear screen
   screen.fill("white")
 
-  # Grass zones
-  pygame.draw.rect(screen, "green", pygame.Rect((0, 0), (600, 50)))
-  pygame.draw.rect(screen, "green", pygame.Rect((0, 350), (600, 50)))
+  # Beginning & End Grass zones
+  pygame.draw.rect(screen, "green", pygame.Rect((0, 0), (600, 40)))
+  pygame.draw.rect(screen, "green", pygame.Rect((0, 360), (600, 40)))
   #Road
-  pygame.draw.rect(screen, "grey", pygame.Rect((0, 50), (600, 300)))
-  pygame.draw.rect(screen, "yellow", pygame.Rect((0, 190), (600, 20)))
+  pygame.draw.rect(screen, "grey", pygame.Rect((0, 40), (600, 320)))
+  #Middle Grass Zone & Road Lines
+  pygame.draw.rect(screen, "green", pygame.Rect((0, 180), (600, 40)))
+  pygame.draw.rect(screen, "yellow", pygame.Rect((0, 100), (600, 20)))
+  pygame.draw.rect(screen, "yellow", pygame.Rect((0, 280), (600, 20)))
+  
 
   #cars
-  pygame.draw.rect(screen, "black", pygame.Rect((0, 65), (100, 50)),10)
-  pygame.draw.rect(screen, "purple", pygame.Rect((0, 125), (200, 50)),10)
-  pygame.draw.rect(screen, "red", pygame.Rect((0, 225), (100, 50)),10)
-  pygame.draw.rect(screen, "blue", pygame.Rect((0, 285), (100, 50)),10)
+  pygame.draw.rect(screen, "black", pygame.Rect((c1_pos[0],c1_pos[1]), (100, 50)))
+  pygame.draw.rect(screen, "purple", pygame.Rect((c2_pos[0],c2_pos[1]), (200, 50)))
+  pygame.draw.rect(screen, "red", pygame.Rect((c3_pos[0],c3_pos[1]), (100, 50)))
+  pygame.draw.rect(screen, "blue", pygame.Rect((c4_pos[0],c4_pos[1]), (100, 50)))
 
   #frog
-  pygame.draw.circle(screen, "#0ba300", (300,375), 25)
+  pygame.draw.circle(screen, "#0ba300", (cur_pos[0],cur_pos[1]), 20)
   
   # update screen
   pygame.display.flip()
