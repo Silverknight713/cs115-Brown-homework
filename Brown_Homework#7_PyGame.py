9c6262574ae6205fa717235106a7e7faa38acbe6
Silverknight713 <silverknight713@gmail.com>
2025-03-13 06:55:58 +0000

    Added score counter, text displaying win / lose, win and lose states, and some functions

--- Brown_Homework#7_PyGame.py
+++ Brown_Homework#7_PyGame.py
@@ -10,6 +10,12 @@ from pygame.constants import KEYDOWN
 #initialize pygame
 pygame.init()
 
+#Fonts
+system_fonts = pygame.font.get_fonts()
+print(system_fonts)
+my_font = pygame.font.SysFont(system_fonts[0], size=20, bold=True, italic=False)
+big_font = pygame.font.SysFont(system_fonts[0], size=60, bold=True, italic=False)
+
 #window dimensions and display
 width = 600
 height = 400
@@ -25,6 +31,7 @@ speed = 10
 cur_pos = [300,380]
 
 #Car Positions
+"""initial car positions"""
 c1_pos = [-100,45] #black car
 c2_pos = [600,125] #purple car
 c3_pos = [-100,225] #red car
@@ -51,7 +58,7 @@ while running:
         cur_pos[0] -= 20
       if event.key == pygame.K_d: # moving right
         cur_pos[0] += 20
-  """Car Movements"""
+  """Car Movements: cars move horizontally in opposite directions, get just off screen, then are reset to the other side of the screen. Reset happens when it detects cars have moved past a certain point."""
   #car1
   if c1_pos[0] > 600:
     c1_pos[0] = -100
@@ -75,6 +82,7 @@ while running:
   """update our game state"""
     
   #update bounds
+  """prevents frog from moving past screen boundaries"""
   if cur_pos[0] < 20: #x direction bounds
     cur_pos[0] = 20
   if cur_pos[0] > width - 20:
@@ -84,46 +92,7 @@ while running:
   if cur_pos[1] > height - 20:
     cur_pos[1] = height - 20
 
-  #Win Condition
-  if cur_pos[1] < 40:
-    print("You Win!")
-    running = False
-  #Lose Condition
-    #Car collision with frog
-  if cur_pos[0] > c1_pos[0] and cur_pos[0] < c1_pos[0] + 100 and cur_pos[1] > c1_pos[1] - 10 and cur_pos[1] < c1_pos[1] + 60:
-    print("YOU LOSE")
-    running = False
-  if cur_pos[0] > c2_pos[0] and cur_pos[0] < c2_pos[0] + 100 and cur_pos[1] > c2_pos[1] - 10 and cur_pos[1] < c2_pos[1] + 60:
-    print("YOU LOSE")
-    running = False
-  if cur_pos[0] > c3_pos[0] and cur_pos[0] < c3_pos[0] + 100 and cur_pos[1] > c3_pos[1] - 10 and cur_pos[1] < c3_pos[1] + 60:
-    print("YOU LOSE")
-    running = False
-  if cur_pos[0] > c4_pos[0] and cur_pos[0] < c4_pos[0] + 100 and cur_pos[1] > c4_pos[1] - 10 and cur_pos[1] < c4_pos[1] + 60:
-    print("YOU LOSE")
-    running = False
   
-  #Score
-  if cur_pos[1] < 40 and score < 1:
-    score += 1
-  if cur_pos[1] < 80 and score < 1:
-    score += 1
-  if cur_pos[1] < 120 and score < 1:
-    score += 1
-  if cur_pos[1] < 160 and score < 1:
-    score += 1
-  if cur_pos[1] < 200 and score < 1:
-    score += 1
-  if cur_pos[1] < 240 and score < 1:
-    score += 1
-  if cur_pos[1] < 280 and score < 1:
-    score += 1
-  if cur_pos[1] < 320 and score < 1:
-    score += 1
-  if cur_pos[1] < 360 and score < 1:
-    score += 1
-  if cur_pos[1] < 400 and score < 1:
-    score += 1
   """ draw to our screen """
   # clear screen
   screen.fill("white")
@@ -152,6 +121,64 @@ while running:
   #frog
   for body in cur_pos:
     pygame.draw.circle(screen, "#0ba300", cur_pos, 20, body)
+
+  #Draw text
+  def draw_text(text, coordinate, text_color, my_font, screen):
+    text_image = my_font.render(text, True, text_color)
+    text_rect = text_image.get_rect()
+    text_rect.topleft = coordinate
+    screen.blit(text_image, text_rect)
+  draw_text(f'Score: {score}', (20,20), "red", my_font, screen)
+
+  #Win Condition
+  """If the frog reaches higher than the last grass patch, win condition is met and game ends"""
+  if cur_pos[1] < 40:
+    print("You Win!")
+    draw_text("YOU WIN!", (150,100), "black", big_font, screen)
+    running = False
+    
+  #Lose Condition
+    #Car collision with frog
+  """when frog position is within the range of the car position, the game ends. done for all 4 cars"""
+  if cur_pos[0] > c1_pos[0] and cur_pos[0] < c1_pos[0] + 100 and cur_pos[1] > c1_pos[1] - 10 and cur_pos[1] < c1_pos[1] + 60:
+    print("YOU LOSE")
+    draw_text("YOU LOSE", (150,100), "red", big_font, screen)
+    running = False
+  if cur_pos[0] > c2_pos[0] and cur_pos[0] < c2_pos[0] + 100 and cur_pos[1] > c2_pos[1] - 10 and cur_pos[1] < c2_pos[1] + 60:
+    print("YOU LOSE")
+    draw_text("YOU LOSE", (150,100), "red", big_font, screen)
+    running = False
+  if cur_pos[0] > c3_pos[0] and cur_pos[0] < c3_pos[0] + 100 and cur_pos[1] > c3_pos[1] - 10 and cur_pos[1] < c3_pos[1] + 60:
+    print("YOU LOSE")
+    draw_text("YOU LOSE", (150,100), "red", big_font, screen)
+    running = False
+  if cur_pos[0] > c4_pos[0] and cur_pos[0] < c4_pos[0] + 100 and cur_pos[1] > c4_pos[1] - 10 and cur_pos[1] < c4_pos[1] + 60:
+    print("YOU LOSE")
+    draw_text("YOU LOSE", (150,100), "red", big_font, screen)
+    running = False
+
+  #Score
+  """score increases based on height, and only increases with further progress"""
+  if cur_pos[1] < 400 and score < 0:
+    score += 1
+  if cur_pos[1] < 360 and score < 1:
+    score += 1
+  if cur_pos[1] < 320 and score < 2:
+    score += 1
+  if cur_pos[1] < 280 and score < 3:
+    score += 1
+  if cur_pos[1] < 240 and score < 4:
+    score += 1
+  if cur_pos[1] < 200 and score < 5:
+    score += 1
+  if cur_pos[1] < 160 and score < 6:
+    score += 1
+  if cur_pos[1] < 120 and score < 7:
+    score += 1
+  if cur_pos[1] < 80 and score < 8:
+    score += 1
+  if cur_pos[1] < 40 and score < 9:
+    score += 1
   
   # update screen
   pygame.display.flip()
